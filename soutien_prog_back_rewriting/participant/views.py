48db4b289ba88ca901025b90107c2a9852c86848
Rached <r.mejri74100@gmail.com>
2021-06-26 01:19:32 +0200

    fix: add permissions to GET method

--- soutien_prog_back_rewriting/participant/views.py
+++ soutien_prog_back_rewriting/participant/views.py
@@ -9,3 +9,11 @@ from .serializers import ParticipantSerializer
 class ParticipantViewSet(ModelViewSet):
     queryset = Participant.objects.all()
     serializer_class = ParticipantSerializer
+
+    def get_permissions(self):
+        permission_classes = []
+
+        if self.action == "read":
+            permission_classes = [IsAuthenticated]
+
+        return [permission() for permission in permission_classes]
