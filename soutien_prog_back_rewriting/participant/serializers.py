65f7c298554012d5139dd3a90635cab4c51be5bd
Rached <r.mejri74100@gmail.com>
2021-06-26 01:16:18 +0200

    fix: fix PrimaryKeyRelatedField POST request

--- soutien_prog_back_rewriting/participant/serializers.py
+++ soutien_prog_back_rewriting/participant/serializers.py
@@ -2,10 +2,11 @@ from rest_framework import serializers
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import permission_classes
 from .models import Participant
+from course.models import Course
 
 
 class ParticipantSerializer(serializers.ModelSerializer):
-    course = serializers.PrimaryKeyRelatedField(many=False, read_only=True)
+    course = serializers.PrimaryKeyRelatedField(many=False, read_only=False, queryset=Course.objects.all())
 
     class Meta:
         model = Participant
