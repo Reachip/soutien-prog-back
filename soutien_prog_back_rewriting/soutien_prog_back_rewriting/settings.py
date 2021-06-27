b56b36498cb58a9f10b91145fbe940d1d5d2c202
Rached <r.mejri74100@gmail.com>
2021-06-28 00:17:04 +0200

    fix: add django-heroku

--- soutien_prog_back_rewriting/soutien_prog_back_rewriting/settings.py
+++ soutien_prog_back_rewriting/soutien_prog_back_rewriting/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -154,3 +155,5 @@ REST_FRAMEWORK = {
     ],
     "DEFAULT_RENDERER_CLASSES": ("rest_framework.renderers.JSONRenderer",),
 }
+
+django_heroku.settings(locals())
