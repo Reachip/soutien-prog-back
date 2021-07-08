544f81363d4d4b39d6e9617976d180aa364403a7
Rached <r.mejri74100@gmail.com>
2021-07-08 16:12:53 +0200

    feat: add DJANGO_DEBUG env. variable

--- soutien_prog_back_rewriting/soutien_prog_back_rewriting/settings.py
+++ soutien_prog_back_rewriting/soutien_prog_back_rewriting/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ["DJANGO_DEBUG"]
 
 # Application definition
 
