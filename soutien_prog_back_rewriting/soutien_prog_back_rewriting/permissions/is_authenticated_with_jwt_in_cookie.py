04e3bdb0ff12b234e8ab823863f4d670c6206dfa
Rached <r.mejri74100@gmail.com>
2021-07-25 08:04:51 +0200

    refactor: initialize an object in one line

--- soutien_prog_back_rewriting/soutien_prog_back_rewriting/permissions/is_authenticated_with_jwt_in_cookie.py
+++ soutien_prog_back_rewriting/soutien_prog_back_rewriting/permissions/is_authenticated_with_jwt_in_cookie.py
@@ -3,10 +3,8 @@ from rest_framework.permissions import BasePermission
 
 class IsAuthenticatedWithJWTInCookie(BasePermission):
     def has_permission(self, request, view):
-        jwt_a = JWTAuthentication()
-
         try:
-            jwt_a.get_validated_token(request.COOKIES["soutienprogtokenaccess"])
+            JWTAuthentication().jwt_a.get_validated_token(request.COOKIES["soutienprogtokenaccess"])
             return True
 
         except Exception:
