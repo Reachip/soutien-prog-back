15e833e81fc5a3b1e18449b44ab0efada04bdc35
Rached <r.mejri74100@gmail.com>
2021-07-27 13:30:06 +0200

    fix: set DJANGO_DEBUG to 1 or 0

--- soutien_prog_back_rewriting/soutien_prog_back_rewriting/settings.py
+++ soutien_prog_back_rewriting/soutien_prog_back_rewriting/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ["DJANGO_DEBUG"]
+DEBUG = True if os.environ["DJANGO_DEBUG"] == 1 else False
 
 # Application definition
 
